41ad837fe31fdf93033a7920410979fa9ffdf429
andrea-crivelli <70567668+andrea-crivelli@users.noreply.github.com>
2020-12-08 11:14:29 +0100

    en-tete methodes vueParticipant

--- wishlist/src/vue/VueParticipant.php
+++ wishlist/src/vue/VueParticipant.php
@@ -6,8 +6,26 @@ namespace wishlist\vue;
 
 class VueParticipant
 {
-    function __construct($objects)
+    private $data;
+
+    public function __construct($data)
     {
+        $this->data=$data;
+    }
+
+    private function affichageListeSouhait()  : string{
+
+    }
+
+    private function andrea2(){
+
+    }
+
+    private function htmlItem(){
+
+    }
+
+    public function affichage(){
 
     }
 }
