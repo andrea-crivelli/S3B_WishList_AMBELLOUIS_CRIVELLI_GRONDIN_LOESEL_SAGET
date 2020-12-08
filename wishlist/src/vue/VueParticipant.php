1e122304b703abf9dcdc3e3854f9986e129730e3
unknown <agathe.loesel3@etu.univ-lorraine.fr>
2020-12-08 11:50:30 +0100

    ajout des methodes de la vue

--- wishlist/src/vue/VueParticipant.php
+++ wishlist/src/vue/VueParticipant.php
@@ -13,19 +13,51 @@ class VueParticipant
         $this->data=$data;
     }
 
-    private function affichageListeSouhait()  : string{
+    private function __affichageListeSouhait()  : string{
 
     }
 
-    private function htmlListeItems(){
+    private function __htmlListeItems(){
 
     }
 
-    private function htmlItem(){
-
+    private function __htmlItem($item){
+        $html = <<<END
+        <section class ="content">
+        <h3>{$item->nom}</h3>
+        <p>{$item->descr}</p>
+        <h4>tarif : {$item->tarif}</h4>
+        </section>
+END;
+        return $html;
     }
 
-    public function affichage(){
 
+    public function affichage($modeAffichage,$item){
+
+        switch ($modeAffichage){
+            case 1 : $this->__affichageListeSouhait();
+            break;
+            case 2 : $this->__htmlListeItems();
+            break;
+            case 3 : $this->__htmlItem($item);
+            break;
+        }
+    }
+
+
+    public function __render(array $vars){
+        $content = $this->__htmlItem($this->data[0]);
+        $html = <<<END 
+        <!DOCTYPE html>
+        <head>
+        <link rel="stylesheet" href="{$vars['basepath']}/wish.css"
+        </head>
+        <body>
+        $content
+</body>
+END;
+
+        return $html;
     }
 }
