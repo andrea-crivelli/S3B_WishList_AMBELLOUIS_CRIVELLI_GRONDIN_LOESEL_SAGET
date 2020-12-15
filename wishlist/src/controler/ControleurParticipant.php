d938beadd19a29e5519863d19152926eb503e3ca
Louis SAGET <louis.saget1@etu.univ-lorraine.fr>
2020-12-15 11:01:55 +0100

    Fin controleur

--- wishlist/src/controler/ControleurParticipant.php
+++ wishlist/src/controler/ControleurParticipant.php
@@ -5,8 +5,8 @@ namespace wishlist\controler;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-use wish\model\Item;
-use wish\view\VueParticipant;
+use wishlist\model\Item;
+use wishlist\view\VueParticipant;
 
 class ControleurParticipant {
 
@@ -19,11 +19,11 @@ class ControleurParticipant {
     public function displayItem(Request $rq, Response $rs, array $args):Response {
         try {
             $item = Item::query()->where('id', '=', $args['id'])->firstOrFail();
-
+            /*
             $url = $this->c->router->pathFor('item', ['id'=>$args['id']]);
 
             $rs->getBody()->write("item : {$item->id} : <a href='$url'>{$item->nom}</a>");
-
+            */
             $htmlvars = ['basepath'=> $rq->getUri()->getBasePath()];
 
             $v = new VueParticipant([$item]);
@@ -32,7 +32,8 @@ class ControleurParticipant {
 
             return $rs;
         } catch (ModelNotFoundException $e) {
-
+            $rs->getBody()->write("item {$item->id} non trouve");
+            return $rs;
         }
     }
 }
