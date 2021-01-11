44de078a13665141fad407b0c16b1314847c014c
andrea-crivelli <70567668+andrea-crivelli@users.noreply.github.com>
2021-01-11 15:36:13 +0100

    ajout routes

--- wishlist/index.php
+++ wishlist/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/src/vendor/autoload.php';
+
 use Illuminate\Database\Capsule\Manager as DB;
 
 use wishlist\modeles\Item;
@@ -13,8 +14,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 //BASE DE DONNEES
-$db=new DB();
-$config=parse_ini_file(__DIR__ . '/conf/db.config.ini');
+$db = new DB();
+$config = parse_ini_file(__DIR__ . '/conf/db.config.ini');
 if ($config) $db->addConnection($config);
 $db->setAsGlobal();
 //demarrer eloquent
@@ -30,30 +31,30 @@ $app = new \Slim\App($container);
 
 // ROUTES SLIM
 
-$app->get('/'               , ControleurPages::class.':pagePrincipale'  )->setName('accueil');
-$app->get('/liste[/]'       , ControleurListe::class.':afficherListes'  )->setName('afficherListes');
-$app->post('/creationListe[/]'  , ControleurListe::class.':creerListe'      )->setName('creationListe');
+$app->get('/', ControleurPages::class . ':pagePrincipale')->setName('accueil');
+$app->get('/liste[/]', ControleurListe::class . ':afficherListes')->setName('afficherListes');
+$app->get('/creationListe[/]', ControleurListe::class . ':afficherFormulaire')->setName('afficherFormulaireCreation');
+$app->post('/creationListe[/]', ControleurListe::class . ':creerListe')->setName('creationListe');
 
 /**
-$app->get('/lists/{id}/items[/]', function (Request $rq, Response $rs, array $args ){
-    $rs->getBody()->write("Affichage des items de la liste {$args['id']}");
-    $list = Liste::find($args['id']);
-    $vue = new VueParticipant($list,2);
-    print($vue->render(array()));
-    return $rs;
-})->setName('afficherItemsListe');
-
-
-
-$app->get('/lists/{token:[a-zA-Z0-9]+[/]}', function (Request $request, Response $response, array $args){
-    $control = new ControleurListe($this->container);
-    return $control->getListe($request, $response, $args);
-})->setName('afficherListe');
-
-$app->get('/items/{id}[/]', function (Request $rq, Response $rs, array $args): Response {
-   $c = new ControleurParticipant($this);
-   return $c->displayItem($rq, $rs, $args);
-})->setName('item');
-
-**/
+ * $app->get('/lists/{id}/items[/]', function (Request $rq, Response $rs, array $args ){
+ * $rs->getBody()->write("Affichage des items de la liste {$args['id']}");
+ * $list = Liste::find($args['id']);
+ * $vue = new VueParticipant($list,2);
+ * print($vue->render(array()));
+ * return $rs;
+ * })->setName('afficherItemsListe');
+ *
+ *
+ *
+ * $app->get('/lists/{token:[a-zA-Z0-9]+[/]}', function (Request $request, Response $response, array $args){
+ * $control = new ControleurListe($this->container);
+ * return $control->getListe($request, $response, $args);
+ * })->setName('afficherListe');
+ *
+ * $app->get('/items/{id}[/]', function (Request $rq, Response $rs, array $args): Response {
+ * $c = new ControleurParticipant($this);
+ * return $c->displayItem($rq, $rs, $args);
+ * })->setName('item');
+ **/
 $app->run();
